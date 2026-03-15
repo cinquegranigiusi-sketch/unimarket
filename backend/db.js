4c913a0ce11fb721e920de942b94d94690d3ccfa
Giusi Cinquegrani <cinquegranigiusi@gmail.com>
2026-03-15 11:13:10 +0100

    connect railway database

--- backend/db.js
+++ backend/db.js
@@ -1,18 +1,19 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "unimarket"
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
 
 db.connect((err) => {
-    if (err) {
-        console.log("Errore di connessione DB", err);
-    } else {
-        console.log("Connesso a MySQL");
-    }
+  if (err) {
+    console.error("Errore connessione database:", err);
+  } else {
+    console.log("Connesso al database Railway");
+  }
 });
 
 module.exports = db;
